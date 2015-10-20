2f2ccba644710bdfc299039b236dd42c0233356b
Vlad <gvandreevich@icloud.com>
2015-10-20 10:29:18 +0300

    by Guskov VA

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += gravitational_constant*body.m*sapce_object.m*(body.y - obj.y)/(r**3)  # FIXME: нужно вывести формулу... (Guskov)
+        body.Fy += gravitational_constant*body.m*sapce_object.m*(body.x - obj.x)/(r**3)   # FIXME: нужно вывести формулу...(Guskov)
 
 
 def move_space_object(body, dt):
@@ -32,9 +32,13 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += (1/2)*ax*(dt**2)  # Guskov
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
+    #Guskov 
+    ay = body.Fy/body.m
+    body.y += (1/2)*ay*(dt**2)  
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
